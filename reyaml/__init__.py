3233335c473248d956620bde450ce1fe77730561
Alex Railean <a.railean@dekart.com>
2014-12-18 17:17:22 +0200

    first commit

---
+++ reyaml/__init__.py
@@ -0,0 +1,35 @@
+import yaml
+
+def load(raw):
+    '''Parses a YAML string, tolerating comments and inline comments, as well as
+    warning you about mixed tabs and spaces'''
+    lines = []
+    spaces = False
+    tabs = False
+
+    for line in raw.splitlines():
+        if line.lstrip().startswith('#'):
+            continue # a line that is a comment (doesn't begin with whitespace)
+        if 0 == len( line.strip() ):
+            continue # this is an empty line, skip it
+        if line.startswith('\t'):
+            tabs = True
+            line = line.replace('\t', '    ')
+        elif line.startswith('  '):
+            spaces = True
+        if spaces and tabs:
+            error = 'You mixed tabs and spaces. Use one or the other, but not both. Problematic line:\n'
+            error += line
+            raise SyntaxError(error)
+
+        #find lines that contain an unquoted URL with an anchor
+        if '#' in line:
+            for part in line.split():
+                if '://' in part and not part[0] in ['\'', '\"']:
+                    error = 'You have a line with a URL and a comment, the comment could be a URL anchor.\n'
+                    error += 'To avoid ambiguities, quote such URLs. Problematic line:\n'
+                    error += line
+                    raise SyntaxError(error)
+        lines.append(line)
+
+    return yaml.load('\n'.join(lines) )
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'reyaml',
+  packages = ['reyaml'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Reader of humane YAML files',
+  author = 'Alex Railean',
+  author_email = 'ralienpp@gmail.com',
+  url = 'https://github.com/ralienpp/reyaml', # use the URL to the github repo
+  download_url = 'https://github.com/ralienpp/reyaml/tarball/0.1', # I'll explain this in a second
+  keywords = ['yaml', 'parser', 'configuration', 'config'], # arbitrary keywords
+  classifiers = [],
+)
