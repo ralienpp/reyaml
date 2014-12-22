2dfabd804214cd636179c13b0fc3466f964f7f06
Alex Railean <a.railean@dekart.com>
2014-12-22 11:50:56 +0200

    missing comma + indentation

--- setup.py
+++ setup.py
@@ -9,28 +9,28 @@ appreciates - comments inside a configuration file. Applies some additional sani
 to the data and fails verbosely, providing a detailed error message.'''
 
 setup(
-  name = 'reyaml',
-  packages = ['reyaml'], # this must be the same as the name above
-  version = '0.2',
-  description=SHORT_DESC,
-  long_description=LONG_DESC,
-  description = 'Reader of humane YAML files',
-  author = 'Alex Railean',
-  author_email = 'ralienpp@gmail.com',
-  url = 'https://github.com/ralienpp/reyaml',
-  download_url = 'https://github.com/ralienpp/reyaml/tarball/0.2',
-  keywords = ['yaml', 'parser', 'configuration', 'config'],
-  license = 'BSD'
-  classifiers = [
-        'Programming Language :: Python',
-        'Operating System :: OS Independent',
-        'Natural Language :: English',
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: BSD License'
-    ],
-  install_requires=[
-      'PyYAML',
-    ]
+    name = 'reyaml',
+    packages = ['reyaml'], # this must be the same as the name above
+    version = '0.2',
+    description=SHORT_DESC,
+    long_description=LONG_DESC,
+    description = 'Reader of humane YAML files',
+    author = 'Alex Railean',
+    author_email = 'ralienpp@gmail.com',
+    url = 'https://github.com/ralienpp/reyaml',
+    download_url = 'https://github.com/ralienpp/reyaml/tarball/0.2',
+    keywords = ['yaml', 'parser', 'configuration', 'config'],
+    license = 'BSD',
+    classifiers = [
+          'Programming Language :: Python',
+          'Operating System :: OS Independent',
+          'Natural Language :: English',
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'License :: OSI Approved :: BSD License'
+      ],
+    install_requires=[
+        'PyYAML',
+      ]
 )
